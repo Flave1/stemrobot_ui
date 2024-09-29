c8088d670c6af0a9eb3e8141fd59f9a3e133baea
flavetechs <104333668+flavetechs@users.noreply.github.com>
2024-09-29 08:04:30 +0100

    modify here section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -11,7 +11,7 @@ const HeroSection = () => (
     
     {/* Heading and Buttons */}
     <h1 className="text-5xl font-bold mb-4 text-center">Innovating Forex & Crypto Trading with AI</h1>
-    <p className="text-lg mb-8 text-center">A reliable trading robots for the forex market. Designed to execute trades efficiently and adapt to market conditions, our robots help traders automate their strategies, maximize returns, and reduce risk.</p>
+    <p className="text-lg mb-8 text-center px-20">A reliable trading robots for the forex market. Designed to execute trades efficiently and adapt to market conditions, our robots help traders automate their strategies, maximize returns, and reduce risk.</p>
     
     {/* Buttons */}
     <div className="space-x-4">
