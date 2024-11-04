3c8271e3a7e00856d7e73896b81287f87da6ef7d
favour@swiftcoretech.com <favour@swiftcoretech.com>
2024-11-04 23:43:05 +0000

    add copilot ui

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,19 +1,22 @@
-import HeroSection from '../components/HeroSection';
-import Features from '../components/Features';
-import Products from '../components/Products';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
-import Navbar from '../components/Navbar';
-
+import { CopilotKit } from "@copilotkit/react-core";
+import HeroSection from "../components/HeroSection";
+import "@copilotkit/react-ui/styles.css";
+import { CopilotPopup } from "@copilotkit/react-ui";
 export default function Index() {
   return (
     <>
-      <Navbar />
-      <HeroSection />
-      <Features />
-      <Products />
-      <Contact />
-      <Footer />
+      <CopilotKit runtimeUrl="/api/copilotkit">
+        <HeroSection />
+        <CopilotPopup
+          instructions={
+            "You are assisting the user as best as you can. Answer in the best way possible given the data you have."
+          }
+          labels={{
+            title: "Popup Assistant",
+            initial: "Need any help?",
+          }}
+        />
+      </CopilotKit>
     </>
   );
 }
