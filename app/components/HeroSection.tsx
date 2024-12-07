92f22923eb1665baf1d8d4a99248fc08b8a4fa03
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-12-07 11:15:50 +0100

    reduced the text size to fit screen for mobile responsiveness

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,10 +1,10 @@
 const HeroSection = () => (
   <section className="font-mont text-white h-screen flex flex-col justify-center items-center hero-bg">
     {/* Heading and Buttons */}
-    <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4 text-center">
+    <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold mb-4 text-center">
       Innovating Forex & Crypto Trading with AI
     </h1>
-    <p className="text-xl mb-8 text-center px-10 lg:text-3xl">
+    <p className="text-xl mb-8 text-center px-10 lg:text-2xl">
       A reliable trading robot for the forex market. Designed to execute trades efficiently and
       adapt to market conditions, our robots help traders automate their strategies, maximize
       returns, and reduce risk.
@@ -13,10 +13,10 @@ const HeroSection = () => (
     <div className="space-x-4">
       <a
         href="/learnmore"
-        className="relative px-8 py-5 rounded group overflow-hidden border-none border-gray-800 bg-transparent transition-all duration-300"
+        className="relative px-8 py-5 rouded-md group overflow-hidden border-none border-gray-800 bg-transparent transition-all duration-300"
       >
         <span
-          className="absolute inset-0 bg-gradient-to-r from-blue-500 via-purple-500 to-blue-900
+          className="absolute inset-0 bg-gradient-to-r from-gray-700 via-purple-500 to-blue-900
                  transition-transform duration-300 transform scale-x-0 group-hover:scale-x-100"
         ></span>
         <span
